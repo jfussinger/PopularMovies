83a7a14a0d1d2933280782a7f1509f7d82d757d5
jfussinger <joseph.fussinger@gmail.com>
2018-03-26 18:42:09 -0400

    Updated API Service.java. Added MovieResponse.java. Updated PopularMovieActivity.java.

---
+++ app/src/main/java/com/example/android/popularmovies/activity/MovieResponse.java
@@ -0,0 +1,53 @@
+package com.example.android.popularmovies.activity;
+
+//https://www.androidhive.info/2016/05/android-working-with-retrofit-http-library/
+
+import android.graphics.Movie;
+
+import com.google.gson.annotations.SerializedName;
+
+import java.util.List;
+
+public class MovieResponse {
+
+    @SerializedName("page")
+    private int page;
+    @SerializedName("results")
+    private List<Movie> results;
+    @SerializedName("total_results")
+    private int totalResults;
+    @SerializedName("total_pages")
+    private int totalPages;
+
+    public int getPage() {
+        return page;
+    }
+
+    public void setPage(int page) {
+        this.page = page;
+    }
+
+    public List<Movie> getResults() {
+        return results;
+    }
+
+    public void setResults(List<Movie> results) {
+        this.results = results;
+    }
+
+    public int getTotalResults() {
+        return totalResults;
+    }
+
+    public void setTotalResults(int totalResults) {
+        this.totalResults = totalResults;
+    }
+
+    public int getTotalPages() {
+        return totalPages;
+    }
+
+    public void setTotalPages(int totalPages) {
+        this.totalPages = totalPages;
+    }
+}
