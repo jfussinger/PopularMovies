039b9c3756b2ddf974c4d9e0e4ff0e226a66861d
jfussinger <joseph.fussinger@gmail.com>
2018-04-07 05:47:44 -0400

    Updated layout height and width for imageView in fragment_popularmovie.xml and fragment_topratedmovie.xml

--- app/src/main/java/com/example/android/popularmovies/adapter/MainAdapter.java
+++ app/src/main/java/com/example/android/popularmovies/adapter/MainAdapter.java
@@ -1,7 +1,6 @@
 package com.example.android.popularmovies.adapter;
 
 import android.content.Context;
-import android.net.Uri;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
